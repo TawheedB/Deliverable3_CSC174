f34d93be3c67d40be1dbad6f2d06a22ede03dd7c
TawheedB <65690320+TawheedB@users.noreply.github.com>
2022-12-02 13:23:46 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-Test
+<?php
+include("config.php");
+
+$query = $connect->query("SELECT * FROM patient);
+?>
+<table border "1">
+  <tr>
+    <td>Patient ID</td>
+    <td>Patient name</td>
+  </tr>
+  <?php
+  while($row = $query->fetch_assoc()){
+    echo "<tr>
+       <td>$no</td>
+       <td>{$row['name']}</td>
+     </tr>";
+     $no++;
+   }
+   ?>
+ </table>
+                         
