3c91a416aee7f93f339019824f70575203d9afdb
TawheedB <65690320+TawheedB@users.noreply.github.com>
2022-12-02 13:53:43 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,21 @@
 <?php
-$db_host ="us-cdbr-east-06.cleardb.net";
-$db_user ="b36704ffd73c9a";
-$db_pass ="cdb199ed";
-$db_name ="heroku_c8a8b19f80392e0";
+  $id = $_POST['id'];
+  $name = $_POST['name'];
 
-$connect = mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die("database conection error");
+
+  $db_host ="us-cdbr-east-06.cleardb.net";
+  $db_user ="b36704ffd73c9a";
+  $db_pass ="cdb199ed";
+  $db_name ="heroku_c8a8b19f80392e0";
+
+  $connect = new mysqli($db_host,$db_user,$db_pass,$db_name);
+  if($connect->connect_error){
+    die("Connection Failed  :  '.$connect->connect_error);
+  }else{
+      $stmt = $connect->prepare("insert into patient(id,name)values(?,?)");
+      $stmt->bind_param("is",$id,$name);
+      echo"registration Completed";
+      $stmt->close();
+      $conn->close();
+  }
+  
