b12b6f0b619b5122a4cea92060e2238efec4feb7
TawheedB <65690320+TawheedB@users.noreply.github.com>
2022-12-02 12:55:32 -0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host ="us-cdbr-east-06.cleardb.net";
+$db_user ="b36704ffd73c9a";
+$db_pass ="cdb199ed";
+$db_name ="heroku_c8a8b19f80392e0";
+
+$connect = mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die("database conection error");
