580fc49544bf4652915b84efd333676dcb2d2d0a
TawheedB <65690320+TawheedB@users.noreply.github.com>
2022-12-02 16:48:31 -0800

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
 		<label for="Pname">Name</label>
 		<input type="text" id="Pname" name="Pname">
 
-		<br>		
+		//<br>		
 
-		<label for="PID">Patient ID#</label>
-		<input type="text" pattern="\d*" name="PID">
+		//<label for="PID">Patient ID#</label>
+		//<input type="text" pattern="\d*" name="PID">
 		
 		<br>
 
@@ -30,7 +30,36 @@
 
 <?php
 
-        $Pname = $_POST["Pname"];
-        $PID = $_POST["PID"];
+$name = $_POST["Pname"];
 
-        print_r($_POST);
+$host = "us-cdbr-east-06.cleardb.net";
+$dbname = "heroku_c8a8b19f80392e0";
+$username = "b36704ffd73c9a";
+$password = "cdb199ed";
+
+$conn = mysqli_connect(hostname: $host,
+                       username: $username,
+                       password: $password,
+                       database: $dbname);
+
+if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO patient (PID, Pname)
+        VALUES (5, ?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+ 
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "s",
+                       $name);
+
+mysqli_stmt_execute($stmt);
+
+
+echo "Record saved.";
