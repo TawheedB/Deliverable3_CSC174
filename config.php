72f6a7691707c9e15bace81a700da04f07f91344
TawheedB <65690320+TawheedB@users.noreply.github.com>
2022-12-02 17:19:55 -0800

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-$host = "us-cdbr-east-06.cleardb.net";
-$dbname = "heroku_c8a8b19f80392e0";
-$username = "b36704ffd73c9a";
-$password = "cdb199ed";
-
-$connect = mysqli_connect($db_hsot,$db_user,$db_pass,$db_name) or die("database connection error");
