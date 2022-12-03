ae358a0d4a78360c2682be6650f111a470704849
TawheedB <65690320+TawheedB@users.noreply.github.com>
2022-12-02 17:05:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -35,6 +35,14 @@
 	$name = $_POST["Pname"];
 	$PID = $_POST["PID"];
 
-	
+	print_r($_POST);	
+$host = "us-cdbr-east-06.cleardb.net";
+$dbname = "heroku_c8a8b19f80392e0";
+$username = "b36704ffd73c9a";
+$password = "cdb199ed";
+
+$connect = mysqli_connect($db_hsot,$db_user,$db_pass,$db_name) or die("database connection error");
+
+
 
 
